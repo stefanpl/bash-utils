25b3d9d0f7a2ffff546438f3a4c307bb562ed978
Stefan Plösser <stefan@rarelime.de>
2019-04-25 09:22:18 +0200

    commands to manage /bin/node links

---
+++ utils/linkNodeBinary.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source ${BASH_UTILS_LOCATION}/logError.sh
+source ${BASH_UTILS_LOCATION}/logInfo.sh
+
+#
+# Creates a link from the default nvm node version to /bin/node
+#
+function linkNodeBinary {
+  binaryPath=/bin/node
+  unlinkNodeBinary || return 1
+  loadNvm || return 1
+  nodePath=`which node`
+  sudo ln -s ${nodePath} ${binaryPath}
+  logInfo "Created link from ${nodePath} to ${binaryPath}"
+}
