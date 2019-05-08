039a580307b25a90e3cbb3277e7caebb0e499b83
Stefan Plösser <stefan@rarelime.de>
2019-05-08 10:29:40 +0200

    send a slack message

---
+++ utils/sendSlackMessage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+###
+# This function will send a message to ${SLACK_HOOK}
+###
+
+source ${BASH_UTILS_LOCATION}/logError.sh
+
+function sendSlackMessage() {
+    if [ -z "$1" ]; then
+      logError "A message must be provided to sendSlackMessage"
+      exit
+    fi
+    username=`id -un`@`uname -n`
+    curl --data "{\"text\": \"${*}\", \"username\": \"${username}\"}" ${SLACK_HOOK}
+}
+
