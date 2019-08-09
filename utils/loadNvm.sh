70c8b5cee754c66579e226d8a7b91944d86cd1f2
Stefan Plösser <stefan@rarelime.de>
2019-08-09 20:20:02 +0200

    error logging for loadNvm

--- utils/loadNvm.sh
+++ utils/loadNvm.sh
@@ -4,6 +4,10 @@
 
 function loadNvm() {
 	if ! [ -x "$(command -v npm)"  ]; then
+		if [ -z "$NVM_DIR" ]; then
+    		logError "\$NVM_DIR is not set. Cannot load nvm."
+				return 1
+		fi
 		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 		[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 	fi
