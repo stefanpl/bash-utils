566ccfe84b4771bd1c77c49e10c05d5e6f154f53
Stefan Plösser <stefan@rarelime.de>
2019-10-28 12:15:07 +0100

    checkout local git branch

---
+++ utils/gitCheckoutLocalMatchingBranch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ${BASH_UTILS_LOCATION}/logError.sh
+source ${BASH_UTILS_LOCATION}/colors.sh
+
+#
+# Checks out a local branch matching a given expression.
+#
+function gitCheckoutLocalMatchingBranch {
+  if [ -z "${1}" ]; then
+    logError "An expression matching a branch must be provided."
+    return 1
+  fi
+  matchingBranch=`git branch | grep -i "${1}"`
+  if [ -z "${matchingBranch}" ]; then
+    logError "Could not find a branch matching '${1}'"
+    return 1
+  fi
+  numberOfMatches=`echo ${matchingBranch} | wc -l`
+  if [ "$numberOfMatches" -gt 1 ]; then
+    logError "Found multiple local branches matching '${1}':"
+    echo ${matchingBranch}
+    return 1
+  fi
+  # xargs is used to trim the branch name
+  git checkout `echo ${matchingBranch} | xargs echo`
+}
