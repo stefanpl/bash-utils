a39d887317160a11f83ce0ae2010322c10810128
Stefan Plösser <stefan@rarelime.de>
2019-12-03 12:10:58 +0100

    bootstrap file

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+if [ -z "$BASH_SOURCE[0]" ]; then
+  scriptDir=`dirname "$(readlink -f "$0")"`
+else
+  scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+fi
+
+if [ -z "$BASH_UTILS_LOCATION"  ]; then
+  echo "No value found for BASH_UTILS_LOCATION. Please set it in your environment." > /dev/stderr
+  return 1
+fi
+
+# Source all files in the utils folder and its subfolders
+source ${BASH_UTILS_LOCATION}/utils/sourceAllShFiles.sh ${BASH_UTILS_LOCATION}
