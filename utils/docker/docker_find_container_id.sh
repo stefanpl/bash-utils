fc4998d8baf089a923fd56c6fc2d660ef41d0904
Stefan Plösser <stefan@rarelime.de>
2020-04-17 09:26:00 +0200

    find container id – possible ––all flag

--- utils/docker/docker_find_container_id.sh
+++ utils/docker/docker_find_container_id.sh
@@ -5,16 +5,24 @@ source ${BASH_UTILS_LOCATION}/utils/logError.sh
 ###
 #
 # Extract the ID from a docker container matching a given string
-# @param $1 – the string which matches a container
+# @param $1 if set to '--all', matching is performed against all containers
+# @param $* – the string which matches a container
 #
 ###
 function docker_find_container_id() {
+    if [ "${1}" = "--all" ]; then
+        allArgument="--all"
+        shift
+    else
+        allArgument=""
+    fi
+    
     if [ -z "$1" ]; then
         logError "Please provide a matching expression as the first command line argument"
         return 1
     fi
 
-    match=`docker container ls | grep -i ${1}`
+    match=`docker container ls ${allArgument} | grep -i ${1}`
     numberOfMatchedLines=`echo ${match} | grep -E -c "^."`
     if [[ $numberOfMatchedLines == "0" ]]; then
         logError "No container found for given expression '${1}'"
