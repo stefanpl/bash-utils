54c49270c55d80f29a4ac66bdcb295161f9d53bc
Stefan Plösser <stefan@rarelime.de>
2020-05-11 10:40:59 +0200

    (un)link node binary

--- utils/linkNodeBinary.sh
+++ utils/linkNodeBinary.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-source ${BASH_UTILS_LOCATION}/utils/logError.sh
-source ${BASH_UTILS_LOCATION}/utils/logInfo.sh
-
 #
 # Creates a link from the default nvm node version to /bin/node
 #
 function linkNodeBinary {
   binaryPath=/bin/node
-  unlinkNodeBinary || return 1
+  unlinkNodeBinary --silent || return 1
   loadNvm || return 1
-  nodePath=`which node`
+  nodePath=`w node`
   sudo ln -s ${nodePath} ${binaryPath}
-  logInfo "Created link from ${nodePath} to ${binaryPath}"
+  logSuccess "Created link from ${nodePath} to ${binaryPath}"
 }
--- utils/unlinkNodeBinary.sh
+++ utils/unlinkNodeBinary.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-source ${BASH_UTILS_LOCATION}/utils/logError.sh
-source ${BASH_UTILS_LOCATION}/utils/logInfo.sh
-
 #
 # If /bin/node is a symbolic link, remove it.
 #
 function unlinkNodeBinary {
   nodeBinary=/bin/node
+  if [ "${1}" = "--silent" ]; then
+    silent=true
+  fi
 
   if [ -h "${nodeBinary}" ]; then
     sudo rm ${nodeBinary}
+    if [ -z "${silent}" ]; then
+      logSuccess "Removed symbolic link ${nodeBinary}"
+    fi
   else 
     if [ -f "${nodeBinary}" ]; then
       logError "${nodeBinary} is a file, but not a symbolic link. Not removing."
       return 1
+    else
+      if [ -z "${silent}" ]; then
+        logInfo "${nodeBinary} not found."
+      fi
     fi
   fi
 }
