3d5790e1341f6ad07ae5c209a660f01992a6bb40
Stefan Plösser <stefan@rarelime.de>
2020-12-22 11:43:39 +0100

    more stuff

---
+++ utils/avaTestMatching.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Searches for variables in a file declared as:
+# variable=value
+# and exports them so they can be used by child scripts.
+
+avaTestMatching() {
+  if [ -z "${1}" ]; then
+    nx ava
+  else
+    nx ava --match "$1"
+  fi
+}
