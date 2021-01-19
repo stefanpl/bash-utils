ac9ec6d9799f85a312f0c79ed340c93548afcec8
Stefan Plösser <stefan@rarelime.de>
2021-01-19 10:09:45 +0100

    toggle capslock extended with argument

--- utils/toggleCapslockBehavior.sh
+++ utils/toggleCapslockBehavior.sh
@@ -5,22 +5,50 @@ source ${BASH_UTILS_LOCATION}/utils/logSuccess.sh
 
 # Toggle capslock behavior between default (capslock) and "swap with escape key"
 
-function toggleCapslockBehavior() {
+toggleCapslockBehavior() {
 
-  function check_if_setxkbmap_is_available() {
+  check_if_setxkbmap_is_available() {
     setxkbmap -version > /dev/null 2>&1 || setxkbmap -help > /dev/null 2>&1 || (logError \
     "setxkbmap does not seem to be installed, but is required for this script. Aborting." ; return 1;)
   }
 
-  function swap_behavior() {
-    if setxkbmap -print | grep "capslock(swapescape)" > /dev/null 2>&1; then
+  isSwapped() {
+    setxkbmap -print | grep "capslock(swapescape)" > /dev/null 2>&1;
+    return $?
+  }
+
+  toggle_behavior() {
+    if isSwapped ; then
+      set_behavior "default"
+    else
+      set_behavior "swap"
+    fi
+  }
+
+  check_for_illegal_argument() {
+    if [ ! -z "${1}" ]; then
+      logError "Unknown argument '${1}'. Use 'swap' or 'default', or none to toggle."
+      return 1
+    fi
+  }
+
+  set_behavior() {
+    if [ "${1}" = "default" ]; then
       setxkbmap -option && \
-      logSuccess "Capslock/escape reverted to default behavior."
+      logSuccess "Capslock/escape set to default behavior."
     else
       setxkbmap -option "caps:swapescape" && \
       logSuccess "Capslock/escape are swapped now!"
     fi
   }
 
-  check_if_setxkbmap_is_available && swap_behavior
+  check_if_setxkbmap_is_available || return
+
+
+  if [ "${1}" = "swap" ] || [ "${1}" = "default" ]; then
+    set_behavior $1
+  else
+    check_for_illegal_argument $1 || return
+    toggle_behavior
+  fi;
 }
