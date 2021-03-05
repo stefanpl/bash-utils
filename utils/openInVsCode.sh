528997db89ab757014508e6d0a2f081cd9c73f0a
Stefan Plösser <stefan@nvon.com>
2021-03-05 04:06:12 +0100

    More dynamic to use code-insiders

--- utils/openInVsCode.sh
+++ utils/openInVsCode.sh
@@ -6,15 +6,20 @@
 # - a .code-workspace file in the current folder
 # - the current folder
 #
-function openInVsCode {
+openInVsCode() {
+  if [ -z "${VSCODE_BINARY}" ]; then
+    local vscodeBinary=code
+  else
+    local vscodeBinary=${VSCODE_BINARY}
+  fi
   # New file:
   if [ ! -z "${1}" ] && [ ! -e "${1}" ]; then
-    code ${1}
+    ${vscodeBinary} ${1}
     return 0
   fi
   # Existing file:
   if [ -f "${1}" ]; then
-    code ${1}
+    ${vscodeBinary} ${1}
     return 0
   fi
   # Directory or no argument at all:
@@ -24,19 +29,19 @@ function openInVsCode {
     folderToOpen=.
   fi
   # Check for workspace file in the folder
-  workspace=`find ${folderToOpen} -maxdepth 1 -mindepth 1 -name "*.code-workspace"`
+  workspace=$(find ${folderToOpen} -maxdepth 1 -mindepth 1 -name "*.code-workspace")
   if [ ! -z "${workspace}" ]; then
     code ${workspace}
     return 0
   fi
   # Check for workspace file in a .vscode folder
   if [ -d "${folderToOpen}/.vscode" ]; then
-    workspace=`find ${folderToOpen}/.vscode -maxdepth 1 -mindepth 1 -name "*.code-workspace" \
-      | awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2- | head -n 1` # sort by line length to take the shortest result
+    workspace=$(find ${folderToOpen}/.vscode -maxdepth 1 -mindepth 1 -name "*.code-workspace" |
+      awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2- | head -n 1) # sort by line length to take the shortest result
     if [ ! -z "${workspace}" ]; then
-      code ${workspace}
+      ${vscodeBinary} ${workspace}
       return 0
     fi
   fi
-  code ${folderToOpen}
+  ${vscodeBinary} ${folderToOpen}
 }
